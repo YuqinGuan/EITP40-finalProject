5c505d02c4d256fed53a419172a29b2a419a0d05
YuqinGuan <yu8160gu-s@student.lu.se>
2023-11-21 17:13:06 +0100

    Update import serial.py

--- notebook/import serial.py
+++ notebook/import serial.py
@@ -15,35 +15,38 @@ width=144
 height=176
 
 def convertToImage(HEXADECIMAL_BYTES):
-     
-     # Reformat the bytes into an image
-     raw_bytes = np.array(HEXADECIMAL_BYTES, dtype="i2")
-     print(raw_bytes)
-     image = np.zeros((len(raw_bytes),3), dtype=int)
+    
+    # Reformat the bytes into an image
+    raw_bytes = np.array(HEXADECIMAL_BYTES, dtype="i2")
+    print(raw_bytes)
+    image = np.zeros((len(raw_bytes),3), dtype=int)
 
-     # Loop through all of the pixels and form the image
-     for i in range(len(raw_bytes)):
-         #Read 16-bit pixel
-         pixel = struct.unpack('>h', raw_bytes[i])[0]
+    # Loop through all of the pixels and form the image
+    for i in range(len(raw_bytes)):
+        #Read 16-bit pixel
+        pixel = struct.unpack('>h', raw_bytes[i])[0]
 
-         #Convert RGB565 to RGB 24-bit
-         r = ((pixel >> 11) & 0x1f) << 3
-         g = ((pixel >> 5) & 0x3f) << 2
-         b = ((pixel >> 0) & 0x1f) << 3
-         image[i] = [r,g,b]
+        #Convert RGB565 to RGB 24-bit
+        r = ((pixel >> 11) & 0x1f) << 3
+        g = ((pixel >> 5) & 0x3f) << 2
+        b = ((pixel >> 0) & 0x1f) << 3
+        image[i] = [r,g,b]
 
-     image = np.reshape(image,(144, 176,3)) #QCIF resolution
+    image = np.reshape(image,(144, 176,3)) #QCIF resolution
 
-     # Show the image
-     plt.imshow(image)
-     plt.show()
+    # Show the image
+    plt.imshow(image)
+    plt.show()
 
 def v2(HEXADECIMAL_BYTES):
-     return Image.frombytes("L", (width, height), HEXADECIMAL_BYTES, decoder_name="xbm")
+    raw_bytes = np.array(HEXADECIMAL_BYTES, dtype="i2")
+    print(raw_bytes)
+    return Image.frombytes("L", (width, height), HEXADECIMAL_BYTES, decoder_name="xbm")
 
 
 def v3(HEXADECIMAL_BYTES):
-   
+    raw_bytes = np.array(HEXADECIMAL_BYTES, dtype="i2")
+    print(raw_bytes)
     img = Image.frombuffer('L', (242,266), HEXADECIMAL_BYTES, 'raw', 'L', 0, 1)
     return img
 
@@ -60,13 +63,28 @@ def main():
     while True:
         ser.flushInput()
         ser.flushOutput()
+        
         cc=str(ser.readline())
         print("String: \n {}\n".format(cc))
         HEXADECIMAL_BYTES=cc[2:][:-5]
+        test=bytes(HEXADECIMAL_BYTES, 'utf-8')
         print("HEX: \n {}\n".format(cc))
         print("HEX length {}".format(len(HEXADECIMAL_BYTES)))
-        imageArray= mapToArray( HEXADECIMAL_BYTES)
-        convertToImage(imageArray)
+        #imageArray= mapToArray( HEXADECIMAL_BYTES)
+        #img=v3(imageArray)
+        #draw = ImageDraw.Draw(img)
+        #img.show()
+        #r_data = binascii.unhexlify(data)
+        #r_data = "".unhexlify(chr(int(b_data[i:i+2],16)) for i in range(0, len(b_data),2))
+        r_data = binascii.unhexlify(test)
+        stream = io.BytesIO(r_data)
+
+        img = Image.open(stream)
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("arial.ttf",14)
+        draw.text((0, 220),"This is a test11",(255,255,0),font=font)
+        draw = ImageDraw.Draw(img)
+        img.save("a_test.png")
     #cc=str(ser.readline())
     #print("String: \n {}\n".format(cc))
     #print("String length :{}".format(len(cc)))
